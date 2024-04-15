cc6c27b1edd8848da06e90c3f12e8a05ebb5208c
신성규 <kxu4583@naver.com>
2024-04-15 20:34:27 +0900

    feat: 싱글톤 웹 드라이버 구현
    설정 파일 이용, 웹드라이버 인스턴스가 하나만 생성되어 공유되도록 함.

---
+++ config.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from chrome_driver_updater import setup_chrome_driver
+
+class Driver:
+    _instance = None
+
+    @staticmethod
+    def get_instance():
+        if Driver._instance is None:
+            Driver._instance = Driver().driver
+        return Driver._instance
+
+    def __init__(self):
+        # 드라이버 설치 및 경로 설정
+        driver_path = setup_chrome_driver()
+
+        # 크롬 옵션을 설정합니다.
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('window-size=1920x1080')
+
+        # 웹 드라이버에 service와 options 값을 전달합니다.
+        s = Service(driver_path)
+        self.driver = webdriver.Chrome(service=s, options=chrome_options)
+
--- inha_portal.py
+++ inha_portal.py
@@ -1,23 +1,12 @@
 import os
 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.service import Service
-from chrome_driver_updater import setup_chrome_driver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import config
 
-# 드라이버 설치 및 경로 설정
-driver_path = setup_chrome_driver()
-
-# 크롬 옵션을 설정합니다.
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('window-size=1920x1080')
-
-# 웹 드라이버에 service와 options 값을 전달합니다.
-s = Service(driver_path)
-driver = webdriver.Chrome(service=s, options=chrome_options)
+driver = config.Driver.get_instance()
 
 # 웹 페이지 열기
 portal_url = "https://portal.inha.ac.kr"
