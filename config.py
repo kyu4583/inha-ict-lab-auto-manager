14da171af4752625cac76361889804e4ebb28da1
신성규 <kxu4583@naver.com>
2024-05-15 16:38:05 +0900

    feat: 크롬 드라이버 헤드리스 모드 on/off 설정 추가
    - 설정은 환경변수를 통해 조작됨.

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
@@ -20,11 +22,17 @@ class Driver:
 
         # 크롬 옵션 설정
         chrome_options = webdriver.ChromeOptions()
-        # 무헤드 모드 추가
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")  # sandbox 비활성화
-        chrome_options.add_argument("--disable-dev-shm-usage")  # /dev/shm 파티션 사용 안 함
-        chrome_options.add_argument("--disable-gpu")  # GPU 가속 비활성화
+
+        # 환경 변수 읽기
+        headless_mode = os.getenv('HEADLESS_MODE', 'True') == 'True'
+
+        if headless_mode:
+            # 무헤드 모드 추가
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--no-sandbox")  # sandbox 비활성화
+            chrome_options.add_argument("--disable-dev-shm-usage")  # /dev/shm 파티션 사용 안 함
+            chrome_options.add_argument("--disable-gpu")  # GPU 가속 비활성화
+
         chrome_options.add_argument('window-size=1920x1080')
 
         # 웹 드라이버에 service와 options 값 전달
