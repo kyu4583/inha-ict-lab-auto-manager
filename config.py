accce9e47b3b58fcfeabed61b46fbddf88f9e3bd
신성규 <kxu4583@naver.com>
2024-05-21 22:13:42 +0900

    feat: 드라이버 생성/소멸 로그 출력 기능 추가

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-
+import uuid  # UUID 모듈 추가
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
@@ -15,10 +15,14 @@ class Driver:
     @staticmethod
     def reset_instance():
         if Driver._instance:
+            print(f"Driver instance {Driver._instance.id} is being reset.")  # 소멸 로그
             Driver._instance.quit()
         Driver._instance = Driver().driver
 
     def __init__(self):
+        # 고유 ID 생성
+        self.id = uuid.uuid4()
+        print(f"Driver instance {self.id} is being created.")  # 생성 로그
 
         # 크롬 옵션 설정
         chrome_options = webdriver.ChromeOptions()
@@ -38,3 +42,7 @@ class Driver:
         # 웹 드라이버에 service와 options 값 전달
         s = Service()
         self.driver = webdriver.Chrome(service=s, options=chrome_options)
+        self.driver.id = self.id  # 드라이버에 ID 할당
+
+    def __del__(self):
+        print(f"Driver instance {self.id} is being destroyed.")  # 소멸 로그
