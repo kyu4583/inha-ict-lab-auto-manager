490def7b525c4d2300198334f17a188c1649cc0e
신성규 <kxu4583@naver.com>
2024-05-22 19:26:57 +0900

    feat: 크롬 브라우저, 드라이버 버전 로그 출력 기능 추가

--- config.py
+++ config.py
@@ -43,5 +43,16 @@ class Driver:
         self.driver = webdriver.Chrome(service=s, options=chrome_options)
         self.driver.id = self.id  # 드라이버에 ID 할당
 
+        # 크롬 버전 및 크롬 드라이버 버전 로그 출력
+        self.print_version_info()
+
+    def print_version_info(self):
+        # 크롬 브라우저 버전
+        browser_version = self.driver.capabilities['browserVersion']
+        # 크롬 드라이버 버전
+        driver_version = self.driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0]
+        print(f"Chrome browser version: {browser_version}")
+        print(f"ChromeDriver version: {driver_version}")
+
     def __del__(self):
         print(f"Driver instance {self.id} is being destroyed.")  # 소멸 로그
